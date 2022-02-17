177805e40fe3b96d114a492029be955d80d52f98
George <george.went@cervusdefence.com>
2022-02-17 16:49:57 +0000

    more docker api additions

--- Python/dockerapi.py
+++ Python/dockerapi.py
@@ -54,6 +54,15 @@ for container in client.containers.list():
     if container.name == "admiring_dewdney":
         print("container exists")
 
+## selecting a specific container v2 - including error handeling! 
+client = docker.from_env()
+try:     
+    container = client.containers.get("jovial_cannon")
+    print("got container: " + str(container))
+    print("also known as: " + str(container.name))
+except:
+    print("no container named jovial_cannon")
+
 ## Selecting containers other than a specific container
 client = docker.from_env()
 for container in client.containers.list():
@@ -73,11 +82,11 @@ for container in client.containers.list():
 # container = client.containers.run("ubuntu:latest", "echo hello world")
 
 ## Run a container in the background
-# print("running containers in the background")
-# client = docker.from_env()
-# container = client.containers.run("bfirsh/reticulate-splines", detach=True)
+print("running containers in the background")
+client = docker.from_env()
+container = client.containers.run("bfirsh/reticulate-splines", detach=True)
 
-## Creating a container from an object
+## Creating a container from an object / dictionary 
 containerObject = {
     "name": "docker_from_object",
     "image": "bfirsh/reticulate-splines",
@@ -89,7 +98,11 @@ print(containerObject["name"])
 client = docker.from_env()
 container = client.containers.run(containerObject["image"], detach=containerObject["detach"])
 
-
+## Making sure we dont delete portainer
+client = docker.from_env()
+container = client.containers.get("portainer")
+print("got container: " + str(container))
+print("also known as: " + str(container.name))
 
 ## MODIFYING EXISTING CONTAINERS
 print("stopping existing containers")
@@ -97,10 +110,12 @@ client = docker.from_env()
 print(client.containers.list())
 for container in client.containers.list():
     print(str(container.name) + "   " + str(container.image))
-    if container.name != "portainer" or "crucible_db_1":
-        print("stopping container" + container.name)
+    if container.image != "portainer/portainer-ce" or "crucible_db_1":
+        print("stopping container: " + container.name)
         container.stop()
-        
+    else: 
+        print("hey this is the portainer container, dont stop it") 
+    
 
     
         
